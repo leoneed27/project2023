e8b733a80c67d318a1689b254e586040adef0afa
leonid <leonid_gom@mail.ru>
2023-02-09 21:36:12 +0300

    first commit

---
+++ project/js/script.js
@@ -0,0 +1,10 @@
+let storeName = "car";
+let storeDescription = {
+    budget : 10000,
+    employess : ["alex", "alin", "ivan"],
+    products : {
+        water : 100,
+        milk : 200,
+    },
+    open : true,
+};
