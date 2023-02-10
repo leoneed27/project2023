caa3bd5fd479ee19dd790ea72dcf040c051e157b
leonid <leonid_gom@mail.ru>
2023-02-10 23:13:56 +0300

    scriptjs mod

--- project/js/script.js
+++ project/js/script.js
@@ -1,10 +1,19 @@
-let storeName = "car";
-let storeDescription = {
-    budget : 10000,
-    employess : ["alex", "alin", "ivan"],
-    products : {
-        water : 100,
-        milk : 200,
-    },
-    open : true,
+const numberOfFilms = +prompt("Сколько вы уже посмотрели фильмов?", '');
+
+const personalMovieDB = {
+    count: numberOfFilms,
+    movies: {},
+    actors: {},
+    genres: [],
+    private: false
 };
+
+const a = prompt("Один из послдених просмотренных фильмов?", ''),
+      b = prompt("На сколько оцените его?", ''),
+      c = prompt("Один из послдених просмотренных фильмов?", ''),
+      d = prompt("На сколько оцените его?", '');
+
+personalMovieDB.movies[a] = b;
+personalMovieDB.movies[c] = d;
+
+console.log(personalMovieDB);
